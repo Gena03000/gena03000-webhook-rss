5bbda83c1e2a1236c004316e462f495b53bf9d0f
Gena03 <162874586+Gena03000@users.noreply.github.com>
2025-07-26 12:14:47 +0200

    Create next.config.js

---
+++ .github/workflows/next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+module.exports = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: '/gena03000-webhook-rss' // si ton dépôt s'appelle comme ça sur GitHub
+};
