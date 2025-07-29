158358ae174e3c300baf702bb29f91da70bb513c
Gena03 <162874586+Gena03000@users.noreply.github.com>
2025-07-29 02:04:53 +0200

    Delete .github/workflows directory

--- .github/workflows/next.config.js
+++
@@ -1,8 +0,0 @@
-// next.config.js
-module.exports = {
-  output: 'export',
-  images: {
-    unoptimized: true
-  },
-  basePath: '/gena03000-webhook-rss' // si ton dépôt s'appelle comme ça sur GitHub
-};
